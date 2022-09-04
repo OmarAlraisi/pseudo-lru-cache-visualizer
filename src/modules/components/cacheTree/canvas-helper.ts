bf48ad6b21a7bdf99e007b1fdc728c7fe169c608
AlRaisi96 <o.raisi@hotmail.com>
2022-09-04 21:30:28 +0400

    Fixes canvas issues.
    - Sets the width and height of canvas based on window width and height.
    - Fits the canvas whenever cache size is changed.

--- src/modules/components/cacheTree/canvas-helper.ts
+++ src/modules/components/cacheTree/canvas-helper.ts
@@ -1,8 +1,12 @@
-import { CacheBit, CacheBlock, DirectionEnum } from "../../../store/types/classes-and-enums-types";
+import {
+  CacheBit,
+  CacheBlock,
+  DirectionEnum,
+} from "../../../store/types/classes-and-enums-types";
 
 export const createNodes = (treeNodes: (CacheBit | CacheBlock)[]) => {
   const nodes = [];
-  let  i = 0;
+  let i = 0;
   for (var node of treeNodes) {
     if (node instanceof CacheBit) {
       nodes.push({
@@ -11,7 +15,7 @@ export const createNodes = (treeNodes: (CacheBit | CacheBlock)[]) => {
         height: 65,
         width: 65,
         disabled: true,
-      })
+      });
     } else {
       nodes.push({
         id: `${i}`,
@@ -20,31 +24,39 @@ export const createNodes = (treeNodes: (CacheBit | CacheBlock)[]) => {
         width: 65,
         className: "block-node",
         disabled: true,
-      })
+      });
     }
     i += 1;
   }
-  console.log(nodes);
   return nodes;
 };
 
-export const createEdges = (treeNodes: (CacheBit | CacheBlock)[], numOfBlocks: number) => {
+export const createEdges = (
+  treeNodes: (CacheBit | CacheBlock)[],
+  numOfBlocks: number
+) => {
   const edges = [];
   for (var i = 0; i < numOfBlocks - 1; i++) {
     edges.push({
-      id: `${i}-${(i * 2) + 1}`,
+      id: `${i}-${i * 2 + 1}`,
       from: `${i}`,
-      to: `${(i * 2) + 1}`,
+      to: `${i * 2 + 1}`,
       disabled: true,
-      className: (treeNodes[i] as CacheBit).direction === DirectionEnum.UP ? "" : "hide-edge",
+      className:
+        (treeNodes[i] as CacheBit).direction === DirectionEnum.UP
+          ? ""
+          : "hide-edge",
     });
     edges.push({
-      id: `${i}-${(i * 2) + 2}`,
+      id: `${i}-${i * 2 + 2}`,
       from: `${i}`,
-      to: `${(i * 2) + 2}`,
+      to: `${i * 2 + 2}`,
       disabled: true,
-      className: (treeNodes[i] as CacheBit).direction === DirectionEnum.UP ? "hide-edge" : "",
+      className:
+        (treeNodes[i] as CacheBit).direction === DirectionEnum.UP
+          ? "hide-edge"
+          : "",
     });
   }
   return edges;
-}
+};
--- src/modules/components/cacheTree/tree.tsx
+++ src/modules/components/cacheTree/tree.tsx
@@ -1,34 +1,45 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import { Canvas, EdgeData, NodeData } from "reaflow";
+import { Canvas, CanvasRef, EdgeData, NodeData } from "reaflow";
 import { AppQueries } from "../../../store/queries/app-queries";
 import { createEdges, createNodes } from "./canvas-helper";
 import "./tree.css";
 
 const CacheTree = () => {
+  let nodes: NodeData[] = [],
+    edges: EdgeData[] = [];
 
-  let nodes: ({ id: string; text: string; height: number; width: number; disabled: boolean; className?: undefined; } | { id: string; text: string; height: number; width: number; className: string; disabled: boolean; })[] | NodeData<any>[] | undefined = [],
-      edges: { id: string; from: string; to: string; disabled: boolean; className: string; }[] | EdgeData<any>[] | undefined = [];
   const numOfBlocks = useSelector(AppQueries.getNumOfBlocks),
-        updateHelper = useSelector(AppQueries.getTreeUpdateHelper),
-        treeNodes = useSelector(AppQueries.getTreeNodes);
+    updateHelper = useSelector(AppQueries.getTreeUpdateHelper),
+    treeNodes = useSelector(AppQueries.getTreeNodes);
 
   useMemo(() => {
     nodes = createNodes(treeNodes);
     edges = createEdges(treeNodes, numOfBlocks);
   }, [updateHelper, treeNodes]);
 
+  useEffect(() => {
+    window.addEventListener("resize", () => {});
+  });
+
+  const ref = useRef<CanvasRef | null>(null);
+
+  useMemo(() => {
+    setTimeout(() => ref.current?.fitCanvas?.(), 150);
+  }, [numOfBlocks]);
+
   return (
-    <div className="cache-tree">
+    <div className="cache-tree--main-root">
       <div>
         <Canvas
           direction="RIGHT"
-          maxWidth={800} // TODO: make dynamic
-          maxHeight={600} // same
+          maxWidth={window.innerWidth - window.innerWidth * 0.3} // TODO: make dynamic
+          maxHeight={window.innerHeight - 120}
           fit={true}
           minZoom={-0.95}
           nodes={nodes}
           edges={edges}
+          ref={ref}
         />
       </div>
     </div>
