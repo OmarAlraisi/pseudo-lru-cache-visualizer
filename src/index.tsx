5d11e03f7660c9018e7767ca32dfc77b876d8473
AlRaisi96 <o.raisi@hotmail.com>
2022-09-08 10:29:06 +0400

    Better hides the app when screen is smaller than recommended

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./modules/App";
 import InvalidScreen from "./modules/invalid-screen";
-import {store} from "./store"
+import { store } from "./store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
